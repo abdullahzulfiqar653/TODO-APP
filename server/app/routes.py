d8f78aee7ff4f03a04a0fc135644e94668a198d7
abdullahzulfiqar785 <abdullahzulfiqar785@gmail.com>
2022-12-03 16:57:54 +0500

    getting all the tasks route added

--- server/app/routes.py
+++ server/app/routes.py
@@ -11,4 +11,15 @@ def create():
     db.session.commit()
     return make_response(jsonify({"id": new_task.id}), 201)
 
+@app.route('/tasks/')
+def get():
+    all_tasks = list()
+    for task in Task.query.all():
+        task_data = dict()
+        task_data['id'] = task.id
+        task_data['name'] = task.name
+        task_data['description'] = task.description
+        task_data['is_completed'] = task.is_completed
+        all_tasks.append(task_data)
 
+    return make_response(jsonify(all_tasks), 200)
