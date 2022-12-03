9f7877859ac77ad6846bd91d6b0fc050e9803754
abdullahzulfiqar785 <abdullahzulfiqar785@gmail.com>
2022-12-04 01:52:33 +0500

    Final Linting applied

--- server/app/utils.py
+++ server/app/utils.py
@@ -1,18 +1,23 @@
+"""this module include the setup to encrypt and decrypt data"""
+
 import base64
-from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
 from main import env_variables
 key=env_variables['KEY']
 
 def encrypt(raw):
+    """taking raw data and converting into string then making cipher using AES"""
     data = str(raw)
     encoded_raw = pad(data.encode(), 16)
     cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
     return base64.b64encode(cipher.encrypt(encoded_raw)).decode('utf-8')
 
 def decrypt(enc):
+    """taking encrypted data and decrypting it using AES"""
     enc = base64.b64decode(enc)
     cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
     return unpad(cipher.decrypt(enc), 16)
 
-# print(decrypt(encrypt("data_here")))
+print(decrypt(encrypt("data_here")))
