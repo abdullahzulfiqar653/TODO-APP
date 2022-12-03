feed9762b91eee8b6174ff32802d6f9a2f8dd7d7
abdullahzulfiqar785 <abdullahzulfiqar785@gmail.com>
2022-12-03 15:57:42 +0500

    A model Task and an endpoint for creating task added

--- server/app/models.py
+++ server/app/models.py
@@ -1,1 +1,7 @@
 from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.Text)
+    is_completed = db.Column(db.Boolean, default=False) 
--- server/app/routes.py
+++ server/app/routes.py
@@ -2,6 +2,13 @@ from app import app, db
 from .models import Task
 from flask import request, make_response, jsonify
 
-@app.route('/')
-def create_task():
-    return make_response(jsonify("Home"), 200)
+
+@app.route('/tasks/create/', methods=['POST'])
+def create():
+    data = request.get_json()
+    new_task = Task(name=data['name'], description=data['description'])
+    db.session.add(new_task)
+    db.session.commit()
+    return make_response(jsonify({"id": new_task.id}), 201)
+
+
